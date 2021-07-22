302eb50903c27185dac60b4260496eb76377b907
sramipastrami <Rsusana24@gmail.com>
2021-07-22 12:54:21 -0700

    create and import TrackList component

--- jamming/src/Components/SearchResults/SearchResults.js
+++ jamming/src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./SearchResults.css";
-// import Tracklist
+
+import TrackList from "../TrackList/TrackList";
 
 class SearchResults extends React.Component {
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        {/* <TrackList /> */}
+        <TrackList />
       </div>
     );
   }
