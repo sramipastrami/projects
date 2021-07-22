fe9eb1bdf34f986553733670e23a13100af68933
sramipastrami <Rsusana24@gmail.com>
2021-07-22 12:25:59 -0700

    create SearchResults component

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
+
 import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
 
 class App extends React.Component {
   render() {
@@ -12,7 +14,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults /> */}
+            <SearchResults />
             {/* <Playlist /> */}
           </div>
         </div>
