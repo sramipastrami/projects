82ccccce54b64b1670676184ae2030a929268683
sramipastrami <Rsusana24@gmail.com>
2021-08-05 10:48:35 -0700

    remove lists in counter.jsx

--- counterapp/src/Components/counter.jsx
+++ counterapp/src/Components/counter.jsx
@@ -6,18 +6,21 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {" "}
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
