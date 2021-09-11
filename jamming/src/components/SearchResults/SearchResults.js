63e40a712814fb1cb806801dffeb43cc7bfda5d2
sramipastrami <Rsusana24@gmail.com>
2021-09-11 16:02:14 -0700

    pass down searchResults and render Results list

--- jamming/src/components/SearchResults/SearchResults.js
+++ jamming/src/components/SearchResults/SearchResults.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./SearchResults.css";
 
-import Tracklist from "../Tracklist/Tracklist";
+import TrackList from "../TrackList/TrackList";
 
 class SearchResults extends React.Component {
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <Tracklist />
+        <TrackList tracks={this.props.searchResults} />
       </div>
     );
   }
--- jamming/src/components/Tracklist/Tracklist.js
+++ jamming/src/components/Tracklist/Tracklist.js
@@ -1,16 +1,18 @@
 import React from "react";
-import "./Tracklist.css";
+import "./TrackList.css";
 
-// import Track from "../Track/Track";
+import Track from "../Track/Track";
 
-class Tracklist extends React.Component {
+class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {this.props.tracks.map((track) => {
+          return <Track track={track} key={track.id} />;
+        })}
       </div>
     );
   }
 }
 
-export default Tracklist;
+export default TrackList;
