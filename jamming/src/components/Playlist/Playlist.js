b850ff7c025634f5fffd6522e753ae6dc7610746
sramipastrami <Rsusana24@gmail.com>
2021-09-10 20:23:02 -0700

    create Tracklist component

--- jamming/src/components/Playlist/Playlist.js
+++ jamming/src/components/Playlist/Playlist.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Playlist.css";
-// import Tracklist from "../Tracklist/Tracklist";
+import Tracklist from "../Tracklist/Tracklist";
 
 class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
         <input defaultValue={"New Playlist"} />
-        {/* <Tracklist /> */}
+        <Tracklist />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
