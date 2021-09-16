ced5f1e46c2bda7cdaea4769d4b83a9172a13d9c
sramipastrami <Rsusana24@gmail.com>
2021-09-16 12:15:46 -0700

    create Spotify file and getAccessToken method

---
+++ jamming/src/util/Spotify.js
@@ -0,0 +1,32 @@
+const clientId = b688a409a16749eb83c020a589293184;
+const redirectUri = "http://localhost:3000/";
+
+let accessToken = "da4255ef7d9f40a684ad2f4891b641d1";
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    //check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      //This clears the parameter, allowing us to grab a new access token when it expires
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  },
+};
+
+export default Spotify;
