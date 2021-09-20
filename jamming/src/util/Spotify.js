b6a521258ec620c7bc2f8639873773f5ae15aebb
sramipastrami <Rsusana24@gmail.com>
2021-09-19 18:48:20 -0700

    create Spotify file and Spotify object

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -1,4 +1,4 @@
-const clientId = b688a409a16749eb83c020a589293184;
+const clientId = "b688a409a16749eb83c020a589293184";
 const redirectUri = "http://localhost:3000/";
 
 let accessToken = "da4255ef7d9f40a684ad2f4891b641d1";
@@ -26,6 +26,30 @@ const Spotify = {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
       window.location = accessUrl;
     }
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+        return jsonResponse.tracks.items.map((track) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri,
+        }));
+      });
   },
 };
 
