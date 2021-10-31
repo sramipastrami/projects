da599b18f0c263057b6170f96b34e41641afb5e6
sramipastrami <Rsusana24@gmail.com>
2021-10-30 20:01:01 -0700

    create contact form

--- appointment-planner/src/components/contactForm/ContactForm.js
+++ appointment-planner/src/components/contactForm/ContactForm.js
@@ -1,15 +1,42 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm = (props) => {
+  const {
+    name,
+    setName,
+    phone,
+    setPhone,
+    email,
+    setEmail,
+    handleSubmit,
+  } = props;
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
   return (
-    ContactForm
+    //  ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={handleNameChange} />
+      <input
+        type="tel"
+        value={phone}
+        onChange={handlePhoneChange}
+        pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+      />
+      <input type="email" value={email} onChange={handleEmailChange} />
+      <button type="submit" text="submit">
+        Submit
+      </button>
+    </form>
   );
 };
