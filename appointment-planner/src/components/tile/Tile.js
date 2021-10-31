df0ca26140c366565a7099a9a08e6c71dede2c00
sramipastrami <Rsusana24@gmail.com>
2021-10-30 21:00:05 -0700

    create tile component

--- appointment-planner/src/components/tile/Tile.js
+++ appointment-planner/src/components/tile/Tile.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ contact }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{contact.name}</p>
+      <p className="tile">{contact.phone}</p>
+      <p className="tile">{contact.email}</p>
     </div>
+
+    // <div className="tile-container">
+    //   {Object.values(contact).map((value, index) => {
+    //     const className = index === 0 ? "tile-title" : "tile";
+    //     return <p className={className}>{value}</p>;
+    //   })}
+    // </div>
   );
 };
