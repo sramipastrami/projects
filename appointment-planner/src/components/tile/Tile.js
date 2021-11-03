3bd1d79f3d90ce6f9c895caea3e3108170534c49
sramipastrami <Rsusana24@gmail.com>
2021-11-03 10:00:22 -0700

    refactor tile component

--- appointment-planner/src/components/tile/Tile.js
+++ appointment-planner/src/components/tile/Tile.js
@@ -1,18 +1,12 @@
 import React from "react";
 
-export const Tile = ({ contact }) => {
+export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
-      <p className="tile-title">{contact.name}</p>
-      <p className="tile">{contact.phone}</p>
-      <p className="tile">{contact.email}</p>
+      {Object.values(tile).map((value, index) => {
+        const className = index === 0 ? "tile-title" : "tile";
+        return <p className={className}>{value}</p>;
+      })}
     </div>
-
-    // <div className="tile-container">
-    //   {Object.values(contact).map((value, index) => {
-    //     const className = index === 0 ? "tile-title" : "tile";
-    //     return <p className={className}>{value}</p>;
-    //   })}
-    // </div>
   );
 };
